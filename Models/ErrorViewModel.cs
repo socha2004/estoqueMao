382ed91cde7933b90902ac3d971c3dfc57c6a4a0
eugenio socha <eugenio.socha2004@gmail.com>
2024-02-14 11:45:13 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EstoqueMao.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
